8bfb1d6bfcb11cddfe8902e2e7a7ef90c079bcb6
timothy vm <timothy.van.meerbeeck@student.ehb.be>
2017-05-15 14:04:36 +0200

    senders done

---
+++ We3/We3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(We3.Startup))]
+namespace We3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
